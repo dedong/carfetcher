dabbbaa740ff063422a5bfd5afe63ce1f8f1ce5e
chalixie <chalixie@chali>
2017-04-10 17:39:45 +0800

    增加部署Tomcat的支持

--- carFetch/src/main/java/com/fei/carFetcher/AppController.java
+++ carFetch/src/main/java/com/fei/carFetcher/AppController.java
@@ -7,9 +7,16 @@ package com.fei.carFetcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppController {
+public class AppController extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AppController.class);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(AppController.class, args);
